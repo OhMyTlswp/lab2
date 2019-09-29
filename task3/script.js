e8bd6d5838b1b486f0a1b7cf3b0ac2d6becc130a
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-09-30 02:04:55 +0300

    Update
    Auto add video

--- task3/script.js
+++ task3/script.js
@@ -1,13 +1,20 @@
 var myIframe = document.getElementsByTagName('iframe');
-var comment = document.querySelectorAll('div.comment-id');
+var comment = document.querySelectorAll('div.comment-block');
 let link = ['https://www.youtube.com/embed/7wB2GVWodiA', 'https://www.youtube.com/embed/Fssfi8ziVdw', 'https://www.youtube.com/embed/fhXQzPDO8d0', 'https://www.youtube.com/embed/vhaqnndhGRw'];
+for (let a = 0; a < link.length; a++) {
+  var btn = document.createElement('button');
+  document.getElementById('men').appendChild(btn);
+  btn.setAttribute('onclick', 'video('+ a +')');
+  var b = a + 1;
+  btn.innerHTML='Видео ' + b;
+};
 function video(e) {
   myIframe[0].src = link[e];
-  if (comment[e].className !== 'comment-id') {
+  if (comment[e].className !== 'comment-block') {
     for (let i = 0; i < comment.length; i++) {
-      comment[i].className = 'comment-id comment-id-none';
+      comment[i].className = 'comment-block none';
     }
-    comment[e].className = 'comment-id';
+    comment[e].className = 'comment-block';
   }
 
 };
