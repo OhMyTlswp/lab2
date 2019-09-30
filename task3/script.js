27f5e2ef8ff0578a92f7318d538fa5a78e439136
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2019-09-30 23:39:20 +0300

    fix

--- task3/script.js
+++ task3/script.js
@@ -1,20 +1,28 @@
-var myIframe = document.getElementsByTagName('iframe');
-var comment = document.querySelectorAll('div.comment-block');
-let link = ['https://www.youtube.com/embed/7wB2GVWodiA', 'https://www.youtube.com/embed/Fssfi8ziVdw', 'https://www.youtube.com/embed/fhXQzPDO8d0', 'https://www.youtube.com/embed/vhaqnndhGRw'];
+var myIframe = document.getElementsByTagName('iframe'),
+comment = document.querySelectorAll('div.comment-block'),
+noComment = document.querySelectorAll('h4.comment-block');
+let link = ['https://www.youtube.com/embed/7wB2GVWodiA', 'https://www.youtube.com/embed/Fssfi8ziVdw', 'https://www.youtube.com/embed/fhXQzPDO8d0', 'https://www.youtube.com/embed/vhaqnndhGRw', 'https://www.youtube.com/embed/Fssfi8ziVdw'];
 for (let a = 0; a < link.length; a++) {
   var btn = document.createElement('button');
-  document.getElementById('men').appendChild(btn);
-  btn.setAttribute('onclick', 'video('+ a +')');
-  var b = a + 1;
-  btn.innerHTML='Видео ' + b;
+  document.getElementById('video-block').appendChild(btn);
+  btn.setAttribute('onclick', 'video(' + a + ')');
+  btn.innerHTML='Видео ' + (a + 1);
 };
 function video(e) {
   myIframe[0].src = link[e];
-  if (comment[e].className !== 'comment-block') {
+  if (comment.length >= e+1) {
     for (let i = 0; i < comment.length; i++) {
-      comment[i].className = 'comment-block none';
+      comment[i].classList.add('none');
     }
-    comment[e].className = 'comment-block';
+    noComment[0].classList.add('none');
+    comment[e].classList.remove('none');
   }
-
+  else {
+    for ( i = 0; i < comment.length; i++) {
+      comment[i].classList.add('none');
+    }
+    noComment[0].classList.remove('none');
+  }
 };
+//Массив для вставки в link
+//['https://www.youtube.com/embed/7wB2GVWodiA', 'https://www.youtube.com/embed/Fssfi8ziVdw', 'https://www.youtube.com/embed/fhXQzPDO8d0', 'https://www.youtube.com/embed/vhaqnndhGRw','https://www.youtube.com/embed/bwMGZtZLRO4','https://www.youtube.com/embed/DkxwgIu1ETA','https://www.youtube.com/embed/8tn2r218r4s'];
